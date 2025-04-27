3f368cabfeaff652b917a5ec980c02e453b88655
Priya Bagus <priyabagus.a29@gmail.com>
2025-04-27 21:59:44 +0700

    preset: update all button presets

--- presets/sakuragi/button/index.js
+++ presets/sakuragi/button/index.js
@@ -1,7 +1,33 @@
-import severityMap from '../severityMap';
-
 export default {
   root: ({ props, context }) => {
+    const severityMap = {
+      base: {
+        primary: 'text-white bg-primary-500 hover:bg-primary-600',
+        secondary: 'text-white bg-secondary-500 hover:bg-secondary-600',
+        danger: 'text-white bg-danger-500 hover:bg-danger-600',
+        success: 'text-white bg-success-500 hover:bg-success-600',
+        warning: 'text-white bg-warning-500 hover:bg-warning-600',
+        info: 'text-white bg-info-500 hover:bg-info-600',
+      },
+      outlined: {
+        primary: 'text-primary-500 ring-primary-500 hover:bg-primary-50',
+        secondary:
+          'text-secondary-500 ring-secondary-500 hover:bg-secondary-50',
+        danger: 'text-danger-500 ring-danger-500 hover:bg-danger-50',
+        success: 'text-success-500 ring-success-500 hover:bg-success-50',
+        warning: 'text-warning-500 ring-warning-500 hover:bg-warning-50',
+        info: 'text-info-500 ring-info-500 hover:bg-info-50',
+      },
+      text: {
+        primary: 'text-primary-500 ring-transparent hover:bg-primary-50',
+        secondary: 'text-secondary-500 ring-transparent hover:bg-secondary-50',
+        danger: 'text-danger-500 ring-transparent hover:bg-danger-50',
+        success: 'text-success-500 ring-transparent hover:bg-success-50',
+        warning: 'text-warning-500 ring-transparent hover:bg-warning-50',
+        info: 'text-info-500 ring-transparent hover:bg-info-50',
+      },
+    };
+
     const severityType = props.text
       ? 'text'
       : props.outlined
