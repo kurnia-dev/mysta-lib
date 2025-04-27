c95cfc673ebd66a72afcb26fd8b1a57f7e626608
Priya Bagus <priyabagus.a29@gmail.com>
2025-04-27 21:56:26 +0700

    preset: update all button presets

--- presets/yuki/button/index.js
+++ presets/yuki/button/index.js
@@ -1,66 +1,80 @@
+import severityMap from '../severityMap';
+
 export default {
-  root: ({ props, context }) => ({
-    className: [
-      'relative',
+  root: ({ props, context }) => {
+    const severityType = props.text
+      ? 'text'
+      : props.outlined
+        ? 'outlined'
+        : 'base';
 
-      `rounded-lg md:w-auto text-xs ${props.severityClass}`,
+    const severityClass = severityMap[severityType][props.severity];
 
-      // Alignments
-      'items-center inline-flex text-center align-bottom justify-center w-max',
-      {
-        'flex-col':
-          (props.iconPos === 'top' || props.iconPos === 'bottom') &&
-          props.label,
+    return {
+      className: [
+        'relative',
+
+        'rounded-lg md:w-auto text-xs',
+        severityClass,
+
+        // Alignments
+        'items-center inline-flex text-center align-bottom justify-center w-max',
+        {
+          'flex-col':
+            (props.iconPos === 'top' || props.iconPos === 'bottom') &&
+            props.label,
+        },
+
+        props.iconPos,
+        {
+          'flex-row-reverse': props.iconPos === 'right',
+        },
+
+        // Sizes & Spacing
+        'leading-none font-medium rounded',
+        {
+          '!text-xs py-[5px] px-4 gap-1': props.label,
+        },
+        {
+          '!w-[26px]': !props.label,
+        },
+
+        // Ring
+        'ring-inset ring-1',
+
+        // Shapes
+        { 'shadow-lg': props.raised },
+
+        // --- Severity Button States ---
+        'focus:outline-none focus:outline-offset-0',
+
+        // Disabled
+        {
+          'pointer-events-none cursor-default': context.disabled,
+          '!text-secondary-300':
+            context.disabled && (props.outlined || props.text),
+          '!ring-secondary-300': context.disabled && props.outlined,
+          '!bg-secondary-300 !text-white !ring-0':
+            context.disabled && !props.outlined && !props.text,
+        },
+
+        // Transitions
+        'transition duration-200 ease-in-out',
+
+        // Misc
+        'cursor-pointer overflow-hidden select-none',
+
+        // Badge
+        '[&>[data-pc-name=badge]]:min-w-4 [&>[data-pc-name=badge]]:h-4 [&>[data-pc-name=badge]]:leading-4',
+      ],
+      style: {
+        height:
+          typeof props.height === 'number' ? `${props.height}px` : props.height,
+        width:
+          typeof props.width === 'number' ? `${props.width}px` : props.width,
       },
-
-      props.iconPos,
-      {
-        'flex-row-reverse': props.iconPos === 'right',
-      },
-
-      // Sizes & Spacing
-      'leading-none font-medium rounded',
-      {
-        '!text-xs py-[5px] !w-full px-4 gap-1': props.label,
-      },
-      {
-        '!w-[26px]': !props.label,
-      },
-
-      // Ring
-      'ring-inset ring-1',
-
-      // Shapes
-      { 'shadow-lg': props.raised },
-
-      // --- Severity Button States ---
-      'focus:outline-none focus:outline-offset-0',
-
-      // Disabled
-      {
-        'pointer-events-none cursor-default': context.disabled,
-        '!text-secondary-300':
-          context.disabled && (props.outlined || props.text),
-        '!ring-secondary-300': context.disabled && props.outlined,
-        '!bg-secondary-300 !text-white !ring-0':
-          context.disabled && !props.outlined && !props.text,
-      },
-
-      // Transitions
-      'transition duration-200 ease-in-out',
-
-      // Misc
-      'cursor-pointer overflow-hidden select-none',
-
-      // Badge
-      '[&>[data-pc-name=badge]]:min-w-4 [&>[data-pc-name=badge]]:h-4 [&>[data-pc-name=badge]]:leading-4',
-    ],
-    style: {
-      height:
-        typeof props.height === 'number' ? `${props.height}px` : props.height,
-      width: typeof props.width === 'number' ? `${props.width}px` : props.width,
-    },
-  }),
+    };
+  },
   label: ({ props }) => ({
     className: [
       'duration-200',
