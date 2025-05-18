4dbf2bbae605c63cfab853fa4ee88ada5d2731b2
Priya Bagus <priyabagus.a29@gmail.com>
2025-05-18 15:52:24 +0700

    feat: implement more type strict for component presets

--- library/hooks/useComponentPreset.ts
+++ library/hooks/useComponentPreset.ts
@@ -1,57 +1,126 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import clsx from 'clsx';
 
+import { ButtonContext, ButtonProps } from 'lib/components/button/Button.d';
+import { CardProps } from 'lib/components/card/Card.d';
+import {
+  CheckboxContext,
+  CheckboxProps,
+} from 'lib/components/checkbox/Checkbox.d';
+import { DialogContext, DialogProps } from 'lib/components/dialog/Dialog.d';
+import { DropdownProps } from 'lib/components/dropdown/Dropdown.d';
+import {
+  FieldWrapperContext,
+  FieldWrapperProps,
+} from 'lib/components/fieldwrapper/FieldWrapper.d';
+import { BaseInputProps } from 'lib/components/private/BaseInput.d';
+import {
+  RadioButtonContext,
+  RadioButtonProps,
+} from 'lib/components/radiobutton/RadioButton.d';
+import {
+  ToggleSwitchContext,
+  ToggleSwitchProps,
+} from 'lib/components/toggleswitch/ToggleSwitch.d';
+import { TooltipProps } from 'lib/components/tooltip/Tooltip.d';
 import { useMystaLib } from 'lib/context/LibContext';
 
-interface PresetAttributes extends React.HTMLAttributes<HTMLElement> {
+type ClassValue = string | Record<string, boolean>;
+
+interface PresetReturn extends React.HTMLAttributes<HTMLElement> {
+  'data-mc-name'?: string;
   'data-mc-section'?: string;
 }
 
-type ClassValue =
-  | string
-  | undefined
-  | null
-  | false
-  | ClassValue[]
-  | Record<string, boolean>;
-
-interface SlotReturn {
-  className?: ClassValue;
-  [key: string]: any;
+interface ComponentPresetMap {
+  Button: {
+    props: ButtonProps;
+    context: ButtonContext;
+  };
+  Card: {
+    props: CardProps;
+    context: never;
+  };
+  Checkbox: {
+    props: CheckboxProps;
+    context: CheckboxContext;
+  };
+  Dialog: {
+    props: DialogProps;
+    context: DialogContext;
+  };
+  Dropdown: {
+    props: DropdownProps;
+    context: never;
+  };
+  FieldWrapper: {
+    props: FieldWrapperProps;
+    context: FieldWrapperContext;
+  };
+  BaseInput: {
+    props: BaseInputProps;
+    context: never;
+  };
+  RadioButton: {
+    props: RadioButtonProps;
+    context: RadioButtonContext;
+  };
+  ToggleSwitch: {
+    props: ToggleSwitchProps;
+    context: ToggleSwitchContext;
+  };
+  Tooltip: {
+    props: TooltipProps;
+    context: never;
+  };
 }
 
-type SlotFunction<T> = (options: T) => SlotReturn;
+export type PresetOptions<C extends keyof ComponentPresetMap = 'Card'> =
+  ComponentPresetMap[C] extends { context: never }
+    ? {
+        props?: Partial<ComponentPresetMap[C]['props']>;
+      }
+    : {
+        context?: ComponentPresetMap[C]['context'];
+        props?: Partial<ComponentPresetMap[C]['props']>;
+      };
 
-type ComponentPreset<T> = {
-  [slotName: string]: SlotFunction<T>;
+export interface PresetAttributes {
+  className: ClassValue | ClassValue[];
+  [key: string]: unknown;
+}
+
+export type PresetMethodAttributes<T> = (options: T) => PresetAttributes;
+
+type ComponentPreset<T extends PresetOptions> = {
+  [slotName: string]: PresetMethodAttributes<T> | PresetAttributes;
 };
 
-export function useComponentPreset<T = Record<string, any>>(
-  componentName: string,
-  options?: T,
-): Record<string, PresetAttributes> {
+export function useComponentPreset<C extends keyof ComponentPresetMap = 'Card'>(
+  componentName: C,
+  options?: PresetOptions<C>,
+): Record<string, PresetReturn> {
   const { preset } = useMystaLib();
   const componentPreset = preset?.[componentName] as
-    | ComponentPreset<T>
+    | ComponentPreset<PresetOptions<C>>
     | undefined;
 
   if (!componentPreset) return {};
 
-  const result: Record<string, PresetAttributes> = {};
+  const result: Record<string, PresetReturn> = {};
 
   for (const slot in componentPreset) {
     const slotFn = componentPreset[slot];
-    if (typeof slotFn === 'function') {
-      const slotAttrs = slotFn(options);
-      const { className: cls, ...rest } = slotAttrs ?? {};
+    const slotAttrs = typeof slotFn === 'function' ? slotFn(options) : slotFn;
+    const { className: cls, ...rest } = slotAttrs ?? {};
 
-      result[slot] = {
-        'data-mc-section': slot,
-        'className': clsx(cls),
-        ...rest,
-      };
-    } else {
-      result[slot] = slotFn;
+    result[slot] = {
+      'className': clsx(cls),
+      'data-mc-section': slot,
+      ...rest,
+    };
+
+    if (slot === 'root') {
+      result[slot]['data-mc-name'] = componentName.toLowerCase();
     }
   }
 
