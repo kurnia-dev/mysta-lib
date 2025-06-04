742bdf667e78922901aa57417f4256a4745ebf07
Priya Bagus <priyabagus.a29@gmail.com>
2025-06-04 09:11:24 +0700

    refactor: adjust Icon to use forwardRef

--- library/components/icon/Icon.tsx
+++ library/components/icon/Icon.tsx
@@ -1,5 +1,5 @@
 import clsx from 'clsx';
-import React, { useRef } from 'react';
+import { ForwardedRef, forwardRef } from 'react';
 
 import { getSeverity } from 'lib/utils';
 
@@ -7,33 +7,31 @@ import { Tooltip } from '../tooltip/Tooltip';
 
 import { IconProps } from './Icon.d';
 
-export const Icon: React.FC<IconProps> = ({
-  name,
-  className,
-  tooltip,
-  tooltipPos,
-  severity,
-  ...rest
-}) => {
-  const ref = useRef<HTMLElement | null>(null);
+export const Icon = forwardRef(
+  (
+    { name, className, tooltip, tooltipPos, severity, ...rest }: IconProps,
+    ref: ForwardedRef<HTMLElement>,
+  ) => {
+    const severityClass = severity
+      ? getSeverity(severity, { color: true })
+      : 'text-black';
 
-  const severityClass = severity
-    ? getSeverity(severity, { color: true })
-    : 'text-black';
+    return (
+      <Tooltip
+        className={className}
+        content={tooltip}
+        hide={!tooltip}
+        position={tooltipPos ?? 'right'}
+      >
+        <i
+          aria-hidden="true"
+          className={clsx('icon', `ic-${name}`, severityClass)}
+          ref={ref}
+          {...rest}
+        />
+      </Tooltip>
+    );
+  },
+);
 
-  return (
-    <Tooltip
-      className={className}
-      content={tooltip}
-      hide={!tooltip}
-      position={tooltipPos ?? 'right'}
-    >
-      <i
-        aria-hidden="true"
-        className={clsx('icon', `ic-${name}`, severityClass)}
-        ref={ref}
-        {...rest}
-      />
-    </Tooltip>
-  );
-};
+Icon.displayName = 'Icon';
